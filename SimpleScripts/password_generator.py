c25c1d3e0cf62018394d1b560a91052d4b5947d8
Pau B.C <blanescolomina.pau@gmail.com>
2022-04-22 00:58:04 +0200

    version 1, only 1 password generated

--- SimpleScripts/password_generator.py
+++ SimpleScripts/password_generator.py
@@ -2,9 +2,15 @@
 import string as st
 import random
 
+password = " "
 # By using the string library constants, we create a list from which we extract the possible combinations
-chars = list(st.ascii_lowercase + st.ascii_uppercase + st.digits + st.punctuation)
+chars = st.ascii_lowercase + st.ascii_uppercase + st.digits + st.punctuation
 
-print(chars)
+print("Introduce the length of the password")
+pass_len = int(input("Minimum 3 characters: "))
 
-# ------------ in progress
+for i in range(0, pass_len):
+    pass_index = random.randrange(0, len(chars))
+    password += chars[pass_index]
+
+print(password)
